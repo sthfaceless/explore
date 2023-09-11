6343db0bb0350e63982b91739dc2a03b96a8cf5a
sthfaceless <sthfaceless@mail.ru>
2023-09-11 10:57:03 +0300

    rgb clip update

--- scripts/dd/shaders/header.hlsl
+++ scripts/dd/shaders/header.hlsl
@@ -21,6 +21,7 @@ static const float PI = 3.14159265f;
 #define GELU2(x) 0.5 * (x) * (1 + tanh(0.7978845608 * min(10, max((x) * (1 + 0.044715 * (x) * (x)), -10))))
 #define GELU(x) (x) * SIGMOID(1.702 * (x))
 #define READ(value, pos, scale) (value) * step(0, (pos).x) * step(0, (pos).y) * step((pos).x, __inputSize.x * scale) * step((pos).y, __inputSize.y * scale)
+#define CLIP01(value) max(min(value, 1), 0)
 
 const static float3x3 rgb2yuv = {
     0.299, 0.587, 0.114,
@@ -49,6 +50,6 @@ void write(float value, int2 pos, float scale) {
     float3 yuv = mul(rgb2yuv, rgb);
     yuv.x = value;
     rgb = mul(yuv2rgb, yuv);
-    WriteToOutput(pos, rgb);
+    WriteToOutput(pos, CLIP01(rgb));
 }
 //!END
